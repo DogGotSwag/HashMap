d987b7107728d34489b981cb53e7e79123f9ceda
Didier <115608922+DogGotSwag@users.noreply.github.com>
2024-10-22 22:18:46 -0600

    setup hash function
    reuse TOP hash function for the time
    being

--- HashMap.js
+++ HashMap.js
@@ -1,7 +1,16 @@
 class HashMap {
   constructor() {}
 
-  hash(key) {}
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode;
+  }
 
   set(key, value) {}
 
