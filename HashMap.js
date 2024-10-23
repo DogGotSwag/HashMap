696d33eb7dc1a75044bf24a655e41345cfbefdf5
Didier <115608922+DogGotSwag@users.noreply.github.com>
2024-10-22 22:24:30 -0600

    apply the modulo operator on each iteration
    too prevent the output from becoming larger than
    our bucket's length

--- HashMap.js
+++ HashMap.js
@@ -1,12 +1,15 @@
 class HashMap {
   constructor() {}
+  #array = [];
+  #bucketLength = 16;
 
   hash(key) {
     let hashCode = 0;
 
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
-      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+      hashCode =
+        (primeNumber * hashCode + key.charCodeAt(i)) % this.#bucketLength;
     }
 
     return hashCode;
